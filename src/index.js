fb080bf0d233da443eaf0c7174e7602a8fe7b314
erik <erik@unswornindustries.com>
2015-06-27 17:41:21 +0200

    ES6 suppor

--- src/index.js
+++ src/index.js
@@ -1,35 +1,36 @@
-var React = require('react');
+import React from 'react';
 
-
-var CloudinaryImage = React.createClass({
+export default class CloudinaryImage extends React.Component {
   propTypes: {
     urlBase: React.PropTypes.string.isRequired,
     id: React.PropTypes.string.isRequired,
     width: React.PropTypes.number,
     height: React.PropTypes.number,
     crop: React.PropTypes.string,
-  },
+  }
 
-  render: function() {
-    var urlParams = [
+  render() {
+    console.log(this.props);
+    const urlParams = [
       this.props.width && 'w_' + this.props.width,
       this.props.height && 'h_' + this.props.height,
       this.props.crop && 'c_' + this.props.crop,
     ];
 
     // Make comma-separated string of existing props ('w_123,h_456')
-    var urlParamsStr = urlParams
+    let urlParamsStr = urlParams
       .filter(function(param) {return param})
       .join(',');
 
     // Add trailing '/' if we have any params  
     urlParamsStr = urlParamsStr && urlParamsStr.concat('/');
       
-    var url = this.props.urlBase + urlParamsStr + this.props.id;
-
-    return React.createElement('img', {src: url}); 
+    const url = this.props.urlBase + urlParamsStr + this.props.id;
+    
+    return (
+      <img src={url} {...this.props} />
+    );
     //return <img src={url} alt="" /> 
   }
-});
+}
 
-module.exports = CloudinaryImage;
